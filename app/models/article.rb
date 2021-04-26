38b290601ba440044d51cfcfc49cb478de9ceaac
Joachim Gonvers <joachim.gonvers@gmail.com>
2021-04-26 20:19:55 +0200

    model association

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
-  belongs_to :new_article
+  belongs_to :old_article, class_name: 'Article'
+  has_one :new_article, class_name: 'Article', foreign_key: "new_article_id"
   belongs_to :maker
 end
